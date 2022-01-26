2422693e6d35ef63951cef4d16130de4cdefc686
DaanvdPoel <200554@student.glu.nl>
2022-01-26 11:39:19 +0100

    .

--- Assets/Scripts/UImanager.cs
+++ Assets/Scripts/UImanager.cs
@@ -5,21 +5,58 @@ using UnityEngine;
 
 public class UImanager : MonoBehaviour
 {
-    [SerializeField] private Text timer; 
-    private float time;
+    [SerializeField] private Text lapTimeText;
+    [SerializeField] private Text highscoreText;
+    [SerializeField] private GameObject scoreboard;
+    [SerializeField] private Text[] scoreboardNumbers;
+
+    private float lapTime;
+    private float highscore;
+
+    private void Awake()
+    {
+        highscore = PlayerPrefs.GetFloat("highscore", 0);
+
+        if (PlayerPrefs.GetFloat("scoreboardNumbers1") == null)
+        {
+            for (int i = 0; i < scoreboardNumbers.Length; i++)
+            {
+                PlayerPrefs.SetFloat("scoreboardNumber" + i, 0);
+            }
+        }
+    }
 
     private void Update()
     {
         raceTimer(true);
     }
 
-    public void raceTimer(bool isRunning) 
+    public void raceTimer(bool isRunning)
     {
-        if(isRunning == true)
+        if (isRunning == true)
         {
-            time = time + Time.deltaTime;
+            lapTime = lapTime + Time.deltaTime;
         }
 
-        timer.text = "Time: " + time.ToString("0.0");
+        lapTimeText.text = "Laptime: " + lapTime.ToString("0.0");
+        highscoreText.text = "Highscore: " + highscore.ToString("0.0");
+    }
+
+    public void HighscoreCheck()
+    {
+        if (lapTime < highscore)
+        {
+            PlayerPrefs.SetFloat("highscore", lapTime);
+        }
+    }
+
+    public void Scoreboard()
+    {
+        scoreboard.SetActive(true);
+
+        for (int i = 0; i < scoreboardNumbers.Length -1; i++)
+        {
+            scoreboardNumbers[i].text = "" + PlayerPrefs.GetFloat("scoreboardNumber" + i);
+        }
     }
 }
